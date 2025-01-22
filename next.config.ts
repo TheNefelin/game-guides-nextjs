928fb4ab9365f23b431f62f460d4ff4fbf7bac7b
Francisco Carmona <flcarmonac@yahoo.com>
2025-01-22 15:36:33 -0300

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dragonra.bsite.net',
+        pathname: '/api/archivo/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
