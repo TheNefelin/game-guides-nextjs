2ac9a336fcc28ce5d0ca28c9ee296904df29a8ad
Francisco Carmona <flcarmonac@yahoo.com>
2025-01-24 00:06:57 -0300

    Update Timeline, Character and Source pages

--- src/app/game/[id]/character/page.tsx
+++ src/app/game/[id]/character/page.tsx
@@ -1,5 +1,21 @@
-export default function CharacterPage() {
+import PageNotFound from "@/components/PageNotFound"
+import { Game } from "@/services/models"
+import Singleton from "@/services/singleton"
+
+interface CharacterPageProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function CharacterPage({ params }: CharacterPageProps) {
+  const { id } = await params
+  if (isNaN(Number(id))) return <PageNotFound/>
+  const gameId: number = Number(id)
+
+  const apiResult = await Singleton.getApiResultAsync()
+  const game: Game | undefined = apiResult.data.find(e => e.id === gameId);
+  if (!game) return <PageNotFound/>
+
   return(
-    <div>CHARACTERS</div>
+    <div>CHARACTERS {gameId}</div>
   )
 }
