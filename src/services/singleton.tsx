5f341838bca560723945fc22a8c4dd8852b9cdb3
Francisco Carmona <flcarmonac@yahoo.com>
2025-01-27 00:31:53 -0300

    update

--- src/services/singleton.tsx
+++ src/services/singleton.tsx
@@ -28,8 +28,8 @@ export default class Singleton {
   private static getInstance(): Singleton {
     if (!this.instance) {
       this.instance = new Singleton();
-      this.apiResult = this.apiFetch();
     }
+    this.apiResult = this.apiFetch();
     return this.instance;
   }
 
@@ -62,7 +62,7 @@ export default class Singleton {
     if (!this.apiResult) {
       this.getInstance();
     }
-    return await (await this.apiResult).data.find(e => e.id === id)
+    return (await this.apiResult).data.find(e => e.id === id)
   }
 
   // Método para obtener la URL completa de la imagen
