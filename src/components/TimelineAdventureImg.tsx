c53e87f3e69c7f47a3705f808324c6ddb6730d46
Francisco Carmona <flcarmonac@yahoo.com>
2025-11-12 07:15:48 -0300

    update

--- src/components/TimelineAdventureImg.tsx
+++ src/components/TimelineAdventureImg.tsx
@@ -8,18 +8,16 @@ interface AdventureImgProps {
 export default function TimelineAdventureImg({ adventureImgs }: AdventureImgProps) {
   return (
     adventureImgs.length > 0 &&
-    <div className='my-4 flex flex-wrap gap-4 justify-center'>
-      {adventureImgs.map((adventureImg: AdventureImg) => (
+      adventureImgs.map((adventureImg: AdventureImg) => (
         <Image
           key={adventureImg.adventureImg_Id}
-          className='shadow-md rounded'
+          className='m-auto p-2 shadow-md rounded'
           src={`/api/getImage?fileName=${adventureImg.imgUrl}`}
           alt="Imagen de Guia"
           width={1536}
           height={1080}
           // style={{ width: 'auto', height: 'auto' }}
         ></Image>
-      ))}
-    </div>
+      ))
   )
 }
